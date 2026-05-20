c06c727be75908f933bf5e7f3f9aea54f1a43d7e
gurkvatten <johanwilhelmkarlsson@gmail.com>
2026-05-20 09:08:15 +0200

    init commit with all services

---
+++ user-service/src/test/kotlin/com/paceai/user/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paceai.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
